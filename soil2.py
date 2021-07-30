68bc6dfcd4685f875c92cf3cc32239a1cb408b60
lin <120934657@qq.com>
2021-07-30 14:19:06 +0800

    soil2

--- soil2.py
+++ soil2.py
@@ -12,14 +12,10 @@ GPIO.setup(4, GPIO.OUT)#物理引脚7对应的BCM编码为4
 while True:
         if GPIO.input(18) == GPIO.LOW:
             GPIO.output(4, GPIO.LOW)
-            print ("土壤：潮湿")
-            print ("信号灯灭")
-            print ("停止洒水")
+            print ("土壤：潮湿 \n信号灯灭 \n停止洒水")
             time.sleep(5) #检测结果潮湿，5秒钟后再次检测土壤湿度
         else:
             GPIO.output(4, GPIO.HIGH)
-            print ("土壤：干燥")
-            print ("信号灯亮")
-            print ("开始洒水")
+            print ("土壤：干燥 \n信号灯亮 \n开始洒水")
             time.sleep(1) #检测结果干燥，启动洒水，每隔1秒检测1次
 
